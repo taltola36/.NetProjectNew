3590a7c1ed735f0a03c0e5f27c18ed1880d08f9b
guitarist684 <avital.hovav@gmail.com>
2016-04-28 17:26:48 +0300

    Game

--- BattleShipWeb/App_Code/Game.cs
+++ BattleShipWeb/App_Code/Game.cs
@@ -24,4 +24,35 @@ public class Game
         else
             return p2.GetBoard();
     }
+
+    public Board createNewBoard(string guid)
+    {
+        if (p1.getGuid().Equals(guid))
+            return p1.createNewBoard();
+        return p2.createNewBoard();
+    }
+
+    public Player getPlayer(int num)
+    {
+        if (num == 1)
+            return p1;
+        return p2;
+    }
+
+    public void removePlayer(int num)
+    {
+        if (num == 1)
+            p1 = null;
+        else
+            p2 = null;
+    }
+
+    public int getNumberofPlayers()
+    {
+        if (p1 == null && p2 == null)
+            return 0;
+        if (p1 != null && p2 != null)
+            return 2;
+        return 1;
+    }
 }
