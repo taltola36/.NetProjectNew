fd86628246aca4d3fe6dc9c5463a65b858ed1edd
guitarist684 <avital.hovav@gmail.com>
2016-04-28 17:27:07 +0300

    Player

--- BattleShipWeb/App_Code/Player.cs
+++ BattleShipWeb/App_Code/Player.cs
@@ -5,11 +5,12 @@ using System.Web;
 
 public class Player
 {
-    private Board myBoard = new Board();
+    private Board myBoard;
     private string myID;
 	
     public Player(string GuID)
     {
+        createNewBoard();
         this.myID = GuID;
     }
 
@@ -20,6 +21,12 @@ public class Player
 
     public Board GetBoard()
     {
+        return myBoard;
+    }
+
+    public Board createNewBoard()
+    {
+        myBoard = new Board();
         return myBoard;
     }
 }
