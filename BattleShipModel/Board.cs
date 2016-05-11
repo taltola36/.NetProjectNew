aa8317dbbf4c1c97f1c451472c94277a50f71437
guitarist684 <avital.hovav@gmail.com>
2016-05-11 22:41:01 +0300

    board

--- BattleShipModel/Board.cs
+++ BattleShipModel/Board.cs
@@ -19,6 +19,8 @@ namespace BattleShipModel
 
         public string Struct { get; set; }
 
+        public int numberOfShipsParts { get; set; }
+
         public int[][] BoardArray { get; set; }
 
     }
