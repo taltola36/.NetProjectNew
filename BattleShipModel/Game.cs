ca8c8d9be446d3ef37296fc05c1837a4fc553b18
guitarist684 <avital.hovav@gmail.com>
2016-05-11 22:45:09 +0300

    Game

--- BattleShipModel/Game.cs
+++ BattleShipModel/Game.cs
@@ -10,9 +10,45 @@ namespace BattleShipModel
     {
         public string ID { get; set; }
 
+        public int playTurn { get; set; }
+
         public Player Player1 { get; set; }
 
         public Player Player2 { get; set; }
+
+        public string MakeMove(int k, int l, string playerId)
+        {
+            string result = "";
+            if (Player1 != null)
+                if (playTurn == 0 && playerId.Equals(Player1.ID))
+                {
+                    result = Player1.Board.BoardArray[k][l].ToString();
+                    if (result.Equals("1"))
+                        Player1.numberOfHits++;
+                }
+            if (Player2 != null)
+                if (playTurn == 1 && playerId.Equals(Player2.ID))
+                {
+                    result = Player2.Board.BoardArray[k][l].ToString();
+                    if (result.Equals("1"))
+                        Player2.numberOfHits++;
+                }
+            if (result.Equals("0"))
+                playTurn = 1 - playTurn;
+            return result;
+        }
+
+        public bool checkEndGame(string playerId)
+        {
+            if (Player1 != null)
+                if (playTurn == 0 && playerId.Equals(Player1.ID))
+                    return (Player1.Board.numberOfShipsParts == Player1.numberOfHits);
+            
+            if (Player2 != null)
+                if (playTurn == 1 && playerId.Equals(Player2.ID))
+                    return (Player2.Board.numberOfShipsParts == Player2.numberOfHits);
+            return false;
+        }
 
         public int NumberOfPlayers
         {
@@ -28,7 +64,6 @@ namespace BattleShipModel
                 {
                     count++;
                 }
-
                 return count;
             }
         }
