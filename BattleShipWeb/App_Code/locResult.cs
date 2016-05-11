2769be2ab8fbe4df0eb9ad48577fc1194d337e72
guitarist684 <avital.hovav@gmail.com>
2016-05-12 00:10:23 +0300

    locResult

---
+++ BattleShipWeb/App_Code/locResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[Serializable]
+public class locResult
+{
+    public string indexes, boardName, isHit;
+
+    public locResult(string indexes, string boardName, string isHit)
+    {
+        this.indexes = indexes;
+        this.boardName = boardName;
+        this.isHit = isHit;
+    }
+}
