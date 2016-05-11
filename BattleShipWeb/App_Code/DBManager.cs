cfb2ff1c7fe25f49a740ac50067cddf93d84de46
guitarist684 <avital.hovav@gmail.com>
2016-05-11 23:08:01 +0300

    DBManager

--- BattleShipWeb/App_Code/DBManager.cs
+++ BattleShipWeb/App_Code/DBManager.cs
@@ -8,7 +8,7 @@ using BattleShipModel;
 
 public class DBManager
 {
-    private const string connectionString = "server=TALBENAMI\\SQLEXPRESS;" +
+    private const string connectionString = "server=AVITALHOVAV\\SQLEXPRESS;" +
                                             "uid=root;" +
                                             "pwd=root; database=.NetProject";
 
