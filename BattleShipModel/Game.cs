35094cc8f2870a4eef209447107d5b1864061e45
guitarist684 <avital.hovav@gmail.com>
2016-05-21 21:18:17 +0300

    2

--- BattleShipModel/Game.cs
+++ BattleShipModel/Game.cs
@@ -16,39 +16,27 @@ namespace BattleShipModel
 
         public Player Player2 { get; set; }
 
-        public string MakeMove(int k, int l, string playerId)
-        {
-            string result = "";
-            if (Player1 != null)
-                if (playTurn == 0 && playerId.Equals(Player1.ID))
-                {
-                    result = Player1.Board.BoardArray[k][l].ToString();
-                    if (result.Equals("1"))
-                        Player1.numberOfHits++;
-                }
-            if (Player2 != null)
-                if (playTurn == 1 && playerId.Equals(Player2.ID))
-                {
-                    result = Player2.Board.BoardArray[k][l].ToString();
-                    if (result.Equals("1"))
-                        Player2.numberOfHits++;
-                }
-            if (result.Equals("0"))
-                playTurn = 1 - playTurn;
-            return result;
-        }
-
-        public bool checkEndGame(string playerId)
-        {
-            if (Player1 != null)
-                if (playTurn == 0 && playerId.Equals(Player1.ID))
-                    return (Player1.Board.numberOfShipsParts == Player1.numberOfHits);
-            
-            if (Player2 != null)
-                if (playTurn == 1 && playerId.Equals(Player2.ID))
-                    return (Player2.Board.numberOfShipsParts == Player2.numberOfHits);
-            return false;
-        }
+        //public string MakeMove(int k, int l, string playerId)
+        //{
+        //    string result = "";
+        //    if (Player1 != null)
+        //        if (playTurn == 0 && playerId.Equals(Player1.ID))
+        //        {
+        //            result = Player1.Board.BoardArray[k][l].ToString();
+        //            if (result.Equals("1"))
+        //                Player1.numberOfHits++;
+        //        }
+        //    if (Player2 != null)
+        //        if (playTurn == 1 && playerId.Equals(Player2.ID))
+        //        {
+        //            result = Player2.Board.BoardArray[k][l].ToString();
+        //            if (result.Equals("1"))
+        //                Player2.numberOfHits++;
+        //        }
+        //    if (result.Equals("0"))
+        //        playTurn = 1 - playTurn;
+        //    return result;
+        //}
 
         public int NumberOfPlayers
         {
