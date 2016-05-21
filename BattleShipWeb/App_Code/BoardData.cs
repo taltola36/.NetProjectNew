d4a8f3d1fefcda82e56cb209612950d067ae7064
guitarist684 <avital.hovav@gmail.com>
2016-05-21 21:19:57 +0300

    9

---
+++ BattleShipWeb/App_Code/BoardData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[Serializable]
+public class BoardData
+{
+    public int[][] boardArr;
+    public string numberOfSubmarinesLeft;
+    public string numberOfAllSubmarines;
+
+    public BoardData(int[][] boardArr, string numberOfSubmarinesLeft, string numberOfAllSubmarines)
+	{
+		this.boardArr = boardArr;
+        this.numberOfSubmarinesLeft = numberOfSubmarinesLeft;
+        this.numberOfAllSubmarines = numberOfAllSubmarines;
+	}
+}
